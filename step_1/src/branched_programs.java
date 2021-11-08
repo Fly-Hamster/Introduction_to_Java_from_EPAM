f9a916875c302e9d24c86b900f3053010be5dacc
Fly-Hamster <malich.s.g@gmail.com>
2021-11-08 18:09:52 +0300

    add 2nd task from branched programs

--- step_1/src/branched_programs.java
+++ step_1/src/branched_programs.java
@@ -17,10 +17,39 @@ public class branched_programs {
             if (first_angle == 90 || second_angle == 90 || third_angle == 90)
                 System.out.println("This triangle is rectangular.");
         }
+    }
+    public static void second_task(){
+        // find: max{min(a, b), min(c, d)}
+        // get options
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a = ");
+        double a = scanner.nextDouble();
+        System.out.print("b = ");
+        double b = scanner.nextDouble();
+        System.out.print("c = ");
+        double c = scanner.nextDouble();
+        System.out.print("d = ");
+        double d = scanner.nextDouble();
+        // calculate
+        double min_a_b, min_c_d, max;
+        if(a >= b) min_a_b = b;
+        else min_a_b = a;
+        if(c >= d) min_c_d = d;
+        else min_c_d = c;
+        if(min_a_b >= min_c_d) max = min_a_b;
+        else max = min_c_d;
+        System.out.println("max{min(a, b), min(c, d)} = " + max);
+
+        // or
+
+        System.out.println("max{min(a, b), min(c, d)} = "
+                + (Math.max(Math.min(a, b), Math.min(c, d))));
+    }
+    public static void third_task(){
 
     }
     public static void main(String[] args) {
         // write your code here
-        first_task();
+        second_task();
     }
 }
